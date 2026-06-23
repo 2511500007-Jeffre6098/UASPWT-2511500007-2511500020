0487b9ab63f523d584c154bd1f1589d4b4963ca2
Widya Serena Mulyaputeri <2511500020@mahasiswa.atmaluhur.ac.id>
2026-06-23 11:02:45 +0700

    DATABASE

--- toko (4).sql
+++ toko (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 17, 2026 at 08:47 AM
+-- Generation Time: Jun 23, 2026 at 04:01 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -31,17 +31,19 @@ CREATE TABLE `barang` (
   `kd_barang` varchar(5) NOT NULL,
   `nm_barang` varchar(30) NOT NULL,
   `harga_barang` int(11) NOT NULL,
-  `kategori_barang` enum('Makanan','Minuman','Sembako','Perawatan pribadi','Kebutuhan rumah tangga','Lain lain') NOT NULL
+  `kategori_barang` enum('Makanan','Minuman','Sembako','Perawatan pribadi','Kebutuhan rumah tangga','Lain lain') NOT NULL,
+  `stok_barang` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `barang`
 --
 
-INSERT INTO `barang` (`kd_barang`, `nm_barang`, `harga_barang`, `kategori_barang`) VALUES
-('B-001', 'Chitato', 12000, 'Makanan'),
-('B-002', 'Cimory', 7000, 'Minuman'),
-('B-003', 'Ultramilk', 7000, 'Minuman');
+INSERT INTO `barang` (`kd_barang`, `nm_barang`, `harga_barang`, `kategori_barang`, `stok_barang`) VALUES
+('B-001', 'Chitato', 12000, 'Makanan', 98),
+('B-002', 'Cimory', 7000, 'Minuman', 96),
+('B-003', 'Ultramilk', 7000, 'Minuman', 98),
+('B-004', 'Loreal Elseve Hair Conditioner', 43000, 'Perawatan pribadi', 39);
 
 -- --------------------------------------------------------
 
@@ -61,8 +63,36 @@ CREATE TABLE `detail_pesanan` (
 --
 
 INSERT INTO `detail_pesanan` (`no_pesanan`, `kd_barang`, `jumlah`, `total`) VALUES
-('P001', 'B-001', 2, 24000),
-('P001', 'B-002', 2, 14000);
+('P007', 'B-004', 1, 43000),
+('P012', 'B-002', 2, 14000),
+('P013', 'B-003', 2, 14000),
+('P013', 'B-001', 2, 24000),
+('P014', 'B-002', 1, 7000),
+('P015', 'B-002', 1, 7000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `no_bayar` varchar(11) NOT NULL,
+  `no_pesanan` varchar(10) NOT NULL,
+  `nominal` int(11) NOT NULL,
+  `kembalian` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`no_bayar`, `no_pesanan`, `nominal`, `kembalian`) VALUES
+('PB007', 'P007', 50000, 7000),
+('PB012', 'P012', 15000, 1000),
+('PB013', 'P013', 50000, 12000),
+('PB014', 'P014', 10000, 3000),
+('PB015', 'P015', 10000, 3000);
 
 -- --------------------------------------------------------
 
@@ -82,7 +112,11 @@ CREATE TABLE `pesanan` (
 --
 
 INSERT INTO `pesanan` (`no_pesanan`, `waktu_pesanan`, `total_pesanan`, `username`) VALUES
-('P001', '2026-06-17 08:46:49', 38000, 'admin');
+('P007', '2026-06-20 13:41:58', 43000, 'admin'),
+('P012', '2026-06-20 13:49:50', 14000, 'admin'),
+('P013', '2026-06-20 14:01:55', 38000, 'admin'),
+('P014', '2026-06-20 14:07:09', 7000, 'admin'),
+('P015', '2026-06-20 14:10:51', 7000, 'admin');
 
 -- --------------------------------------------------------
 
@@ -106,6 +140,12 @@ INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`no_bayar`);
 
 --
 -- Indexes for table `user`
